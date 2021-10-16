fe3ea0a0bc9557128e9628b24d09c7a759266413
KDace <79869230+KDaceyx@users.noreply.github.com>
2021-10-15 23:05:27 -0400

    fixed API

--- Project_Files/app.py
+++ Project_Files/app.py
@@ -1,5 +1,6 @@
 #dependencies
 import numpy as np
+import json
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
@@ -12,7 +13,7 @@ from flask import Flask, jsonify
 # Database Setup
 #################################################
 rds_connection_string = "postgres:VinceNoir@localhost:5432/amc"
-engine = create_engine(f'postgresql://{rds_connection_string}')
+engine = create_engine(f'postgres://{rds_connection_string}')
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -37,77 +38,69 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/amc<br/>"
-        f"/api/v1.0/nasdaq"
+        f"/api/v1.0/amc/Date<br/>"
+        f"/api/v1.0/amc/Close<br/>"
+        f"/api/v1.0/amc/Volume<br/>"
+        f"/api/v1.0/nasdaq/Date<br/>"
+        f"/api/v1.0/nasdaq/Close<br/>"
+        f"/api/v1.0/nasdaq/Volume"
+
     )
 
-@app.route("/api/v1.0/amc")
-def amc():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    """Return all stock data for amc"""
-    #Query all stock data
-    amc_results = session.query(amc).all()
-
-    session.close()
-
 @app.route("/api/v1.0/amc/Date")
-def date():
+def amc_date():
     session = Session(engine)
 
     date_results = session.query(amc.Date).all()
 
     session.close()
+    return jsonify(date_results)
+    
 
 @app.route("/api/v1.0/amc/Close")
-def close():
+def amc_close():
     session = Session(engine)
 
     close_results = session.query(amc.Close).all()
 
     session.close()
+    return jsonify(close_results)
 
 @app.route("/api/v1.0/amc/Volume")
-def volume():
+def amc_volume():
     session = Session(engine)
 
     volume_results = session.query(amc.Volume).all()
 
     session.close()
-
-@app.route("/api/v1.0/nasdaq")
-def amc():
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-
-    """Return all stock data for nasdaq"""
-    #Query all stock data
-    nasdaq_results = session.query(nasdaq).all()
-
-    session.close()
+    return jsonify(volume_results)
 
 @app.route("/api/v1.0/nasdaq/Date")
-def date():
+def n_date():
     session = Session(engine)
 
-    date_results = session.query(nasdaq.Date).all()
+    n_date_results = session.query(nasdaq.Date).all()
 
     session.close()
+    return jsonify(n_date_results)
 
 @app.route("/api/v1.0/nasdaq/Close")
-def close():
+def n_close():
     session = Session(engine)
 
-    close_results = session.query(nasdaq.Close).all()
+    n_close_results = session.query(nasdaq.Close).all()
 
     session.close()
+    return jsonify(n_close_results)
 
 @app.route("/api/v1.0/nasdaq/Volume")
-def volume():
+def n_volume():
     session = Session(engine)
 
-    volume_results = session.query(nasdaq.Volume).all()
+    n_volume_results = session.query(nasdaq.Volume).all()
 
     session.close()
+    return jsonify(n_volume_results)
 
+if __name__ == '__main__':
+    app.run(debug=True)
